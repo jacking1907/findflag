6b515a565f48902ddc35f52882111b9af80a7087
FriedrichRavaDev <161723148+FriedrichRavaDev@users.noreply.github.com>
2024-05-08 16:31:37 +0200

    Update services.cs
    Aggiunte due funzioni già esistenti su un'altro programma

--- eseFunzioniBasket/services.cs
+++ eseFunzioniBasket/services.cs
@@ -21,6 +21,43 @@ namespace servizi
                 outString += actForm.Controls[$"textBoxL{line}C{i}"].Text;
             }
             return outString; //ritorna la parola intera ciclando le colonne, sapendo la riga
+
+ public static bool GetIfTextBoxIsEmpty(System.Windows.Forms.Form actForm, int line)
+ {
+     int x = 0;
+
+     for (int i = 1; i <= 6; i++)
+     {
+         if(actForm.Controls[$"textBoxL{line}C{i}"].Text != "")
+         {
+             x++;
+         }
+     }
+
+     if(x==6) 
+     { return true; }
+
+     return false;
+ }
+
+ public static bool NotEnabled(System.Windows.Forms.Form actForm, int line)
+ {
+     int x = 0;
+
+     for (int i = 1; i <= 6; i++)
+     {
+         if (actForm.Controls[$"textBoxL{line}C{i}"].Enabled == true)
+         {
+             (actForm.Controls[$"textBoxL{line}C{i}"].Enabled) = false;
+             x++;
+         }
+     }
+
+     if (x == 6)
+     { return true; }
+
+     return false;
+ }
             
         }
 
